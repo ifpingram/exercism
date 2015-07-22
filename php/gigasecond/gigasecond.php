c4552f3a6611624cc1a11537589b91e334accc03
Ingram Monk <ingram.monk@gmail.com>
2015-07-22 20:38:23 +0100

    working on php raindrops

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -2,26 +2,5 @@
 
 function from($start_date)
 {
-    return new Gigasecond($start_date);
-}
-
-class Gigasecond
-{
-    private $date;
-
-    public function __construct(DateTime $start_date)
-    {
-        $this->date = $start_date;
-        $this->calculate_gigasecond();
-    }
-
-    public function calculate_gigasecond()
-    {
-        $this->date->add(new DateInterval('PT1000000000S'));
-    }
-
-    public function format($format)
-    {
-        return $this->date->format($format);
-    }
+    return $start_date->add(new DateInterval('PT1000000000S'));
 }
